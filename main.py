5dc55846b7e6283a9dc6b05bd146816ccf209d86
UmarCoder-604 <u2022604@giki.edu.pk>
2024-03-26 21:06:45 +0500

    Update main.py
    Just made a GUI window

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("hello world")
-print("Hello. I am M Zeeshan Khan. \n -group member of the Sem Project.")
+# print("hello world")
+# print("Hello. I am M Zeeshan Khan. \n -group member of the Sem Project.")
+
+from tkinter import *
+
+dbms_root = Tk()
+
+dbms_root.geometry("600x485")
+dbms_root.minsize(300,100)
+donation=Label(text="BLOOD DONATION PROJECT")
+donation.pack()
+
+dbms_root.mainloop()
+

26b8579fd1105db2268f6586300f689f96469803
N00Rain <161249711+N00Rain@users.noreply.github.com>
2024-04-25 16:15:23 +0500

    Trigger function to update blood inventory

--- database.py
+++ database.py
@@ -77,6 +77,51 @@ def insert_blood_bank():
             cursor.close()
             db.close()
 
+#trigger fuction for blood inventory
+def after_update_blood_inventory():
+    db = get_db_connection()
+    if db:
+        try:
+            cursor = db.cursor()
+           
+            trigger_query = """
+                CREATE OR REPLACE FUNCTION after_update_blood_inventory_trigger()
+                RETURNS TRIGGER AS $$
+                DECLARE
+                    blood_type_threshold INT := 100; 
+                    blood_expiry_period INTERVAL := '90 days';  
+                BEGIN
+                    -- if the quantity of any blood type falls below the threshold
+                    IF NEW.quantity < blood_type_threshold THEN
+                       
+                        RAISE NOTICE 'Alert: Quantity of blood type % is below threshold', NEW.blood_type;
+                    END IF;
+
+                    
+                    UPDATE blood_inventory
+                    SET expiry_date = NEW.date_collected + blood_expiry_period
+                    WHERE blood_id = NEW.blood_id;
+
+                    RETURN NEW;
+                END;
+                $$ LANGUAGE plpgsql;
+                
+                CREATE TRIGGER blood_inventory_after_update_trigger
+                AFTER UPDATE ON blood_inventory
+                FOR EACH ROW
+                EXECUTE FUNCTION after_update_blood_inventory();
+            """
+            cursor.execute(trigger_query)
+            db.commit()
+            
+        except psycopg2.Error as e:
+            print("Error creating trigger:", e)
+            db.rollback()
+        finally:
+            # Close the cursor and database connection
+            cursor.close()
+            db.close()
+
 def insert_donations():
     donation_id = input("Enter donation ID : ")
     donor_id = input("Enter donor's ID : ")
@@ -128,6 +173,8 @@ def insert_donations():
             # Close the cursor and database connection
             cursor.close()
             db.close()
+
+
 
 def view_table_data(table_name):
     db = get_db_connection()

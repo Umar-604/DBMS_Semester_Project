4880f4c990a95db13e2161bc26752901e59818fa
Muhammad Zeeshan Khan <133538176+okzeesh@users.noreply.github.com>
2024-05-09 15:22:36 +0500

    Update database.py
    created a function to view records of donation table based o the donor id.

--- database.py
+++ database.py
@@ -296,6 +296,24 @@ def view_donor(donor_id):
         db.close()
         return donor_records
 
+def view_donation(donor_id):
+    db = get_db_connection()
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT * FROM donations WHERE donor_id = %s", (donor_id,))
+        donation_records = cursor.fetchall()
+        if donation_records:
+            for record in donation_records:
+                print(record)  # Or process the records as needed
+        else:
+            print("No records found for donor ID:", donor_id)
+    except psycopg2.Error as e:
+        print("Error viewing donor records:", e)
+    finally:
+        cursor.close()
+        db.close()
+        return donation_records
+
 def view_table_data(table_name):
     db = get_db_connection()
     cursor = db.cursor()

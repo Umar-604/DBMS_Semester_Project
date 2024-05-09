27de391eb546a9b6e8f331f122831b1a8ffbbbd2
N00Rain <161249711+N00Rain@users.noreply.github.com>
2024-05-09 22:49:18 +0500

    donation options window

--- donor_page.py
+++ donor_page.py
@@ -6,3 +6,7 @@ from database import *
 class DonorGUI:
     def _init_(self, root):
         self.donation_options_window = root
+
+# Create the donation options window
+        self.donation_options_window.title("Donation Options")
+        self.donation_options_window.geometry("500x500")

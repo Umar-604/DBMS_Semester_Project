90d71ad85fdc072acf354f8c6e222c59679db5ee
N00Rain <161249711+N00Rain@users.noreply.github.com>
2024-05-09 22:48:17 +0500

    donorGUI

---
+++ donor_page.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk  
+from database import *
+
+class DonorGUI:
+    def _init_(self, root):
+        self.donation_options_window = root

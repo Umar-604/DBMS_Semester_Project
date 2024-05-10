fcbac589cd584cddba71b210f8f898d46b70c693
Muhammad Zeeshan Khan <133538176+okzeesh@users.noreply.github.com>
2024-05-10 21:52:50 +0500

    Update database.py
    added insertion of receiver to firebase

--- database.py
+++ database.py
@@ -77,11 +77,10 @@ def insert_receiver(recipient_id, name, contact, blood_type, date_of_birth, gend
                            (recipient_id, name, contact, blood_type, date_of_birth, gender, health_condition, hospital))
             db.commit()
             print("Receiver's data inserted successfully into PostgreSQL!")
-
+            
             # Insert data into Firebase
-            insert_receiver_firebase(recipient_id, name, contact, blood_type, date_of_birth, gender, health_condition,
-                                     hospital)
-
+            insert_receiver_firebase(recipient_id, name, contact, blood_type, date_of_birth, gender, health_condition, hospital)
+            
         except psycopg2.Error as e:
             print("Error inserting recipient data into PostgreSQL:", e)
             db.rollback()
@@ -89,6 +88,19 @@ def insert_receiver(recipient_id, name, contact, blood_type, date_of_birth, gend
             # Close the cursor and database connection
             cursor.close()
             db.close()
+
+def insert_receiver_firebase(recipient_id, name, contact, blood_type, date_of_birth, gender, health_condition, hospital):
+    ref = db.reference('recipients')  # Reference to the 'recipients' node in your Firebase database
+    ref.child(recipient_id).set({
+        'name': name,
+        'contact': contact,
+        'blood_type': blood_type,
+        'date_of_birth': date_of_birth,
+        'gender': gender,
+        'health_condition': health_condition,
+        'hospital': hospital
+    })
+
 
 def insert_blood_bank(blood_bank_id, name, location, contact, services_provided, operating_hours):
     # Insert into PostgreSQL
